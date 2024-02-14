b2e9dd199686e577f38f604826e7985b4f820d8f
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-02-14 14:43:39 +0000

    Make top pages

--- test/controllers/top_pages_controller_test.rb
+++ test/controllers/top_pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TopPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get top_pages_home_url
+    get root_path
     assert_response :success
   end
 end
