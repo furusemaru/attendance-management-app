936a6b2b070d0cc3b2674eb929ab437c163a6d6a
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-02-15 15:23:38 +0000

    Make top page

--- test/controllers/top_pages_controller_test.rb
+++ test/controllers/top_pages_controller_test.rb
@@ -4,5 +4,7 @@ class TopPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
+    assert_template "top_pages/home"
+    assert_select 'title', '勤怠管理'
   end
 end
