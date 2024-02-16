0a1351662b356cd962eaccf5e0b4d12b3db5147c
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-02-16 11:13:03 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ログインに成功しました'
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_cofirmation)
+    end
 end
