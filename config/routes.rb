51a3c8b20e09319721376057c088b85bac4ea06a
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-02-16 15:26:26 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'signup', to: "users#new"
+  get '/signup', to: "users#new"
   root "top_pages#home"
   resources :users
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
   
 end
