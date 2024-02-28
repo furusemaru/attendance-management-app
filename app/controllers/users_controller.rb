092468f3481abbf614819fdaa24dee7a04f45b0a
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-02-28 10:37:24 +0000

    ユーザー作成後の遷移を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = '作成に成功しました'
-      redirect_to @user
+      redirect_to users_url
     else
       render 'new', status: :unprocessable_entity
     end
