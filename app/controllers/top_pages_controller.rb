59a20917b815ac46e4eb99eefe289f36313b1a41
furusemaru <138269691+furusemaru@users.noreply.github.com>
2024-02-28 12:47:03 +0000

    アラート画面に残業時間による制限を加えました

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -3,7 +3,8 @@ class TopPagesController < ApplicationController
     if logged_in?
       @work = current_user.works.build 
       if current_user.admin?
-        @users = User.all.select { |user| user.overtime_this_month > 126000 }
+        @alert_time = params[:over_time].nil? ? 126000 : params[:over_time].to_i*3600 
+        @users = User.all.select { |user| user.overtime_this_month > @alert_time }
         @users = User.where(id: @users.map(&:id)).paginate(page: params[:page])
         @this_month = Date.today.month < 10 ? '0' + Date.today.month.to_s : Date.today.month.to_s 
         @this_year = Date.today.year.to_s
